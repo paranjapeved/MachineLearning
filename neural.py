47a641eb22f792e02d92fd2ac41b72b90818e556
paranjapeved <paranjapeved@gmail.com>
2018-01-11 12:05:34 +0530

    Added neural network

--- neural.py
+++ neural.py
@@ -1,7 +1,31 @@
+#Author: Ved Paranjape
+#Neural Network
+# The following is a neural network for classifying iris data. The parameters under consideration are petal length and
+# petal width. The network is trained using leave-one-out analysis. The dataset consists of 100 samples. The training
+# is done using 99 samples and the remaining sample is used for testing. This process is repeated. The model works with
+# an accuracy of 94 %.
+
 import numpy as np
-import math
-import random
-from matplotlib import pyplot as plt
+from sklearn import datasets
+
+
+def test_classify(hidden_n,X, theta, output_vars,bias_hidden,bias_output):
+    # forward propogation
+
+    # for hidden layer
+    hiddenlayer_nobias = np.dot(X, theta[1])
+    # adding bias to input
+    hiddenlayer_inputs = hiddenlayer_nobias + bias_hidden
+    hiddenlayer_outputs = sigmoid(hiddenlayer_inputs)
+
+    # for output layer
+    outputlayer_inputs_nobias = np.dot(hiddenlayer_outputs, theta[2])
+
+    # adding bias to output
+    outputlayer_inputs = outputlayer_inputs_nobias + bias_output
+    outputlayer_outputs = sigmoid(outputlayer_inputs)
+
+    return outputlayer_outputs[0]
 
 #sigmoid derivative function
 def sigmoid_derivative(a):
@@ -17,8 +41,6 @@ def classify(iters, hidden_n, num_of_samples, X, theta, output_vars, y,bias_hidd
     J_theta = []
     m = len(X)
     p = []
-    theta_1 = {10:[],11:[],12:[],20:[],21:[],22:[]}
-    theta_2 = {10:[],11:[],12:[],20:[],21:[],22:[]}
     for i in range(iters):
 
         # forward propogation
@@ -35,19 +57,17 @@ def classify(iters, hidden_n, num_of_samples, X, theta, output_vars, y,bias_hidd
         #adding bias to output
         outputlayer_inputs = outputlayer_inputs_nobias + bias_output
         outputlayer_outputs = sigmoid(outputlayer_inputs)
-        #J_theta.append((-1/m)*np.sum((y * np.log(outputlayer_outputs) + (1-y) * np.log(1-outputlayer_outputs))))
-        J_theta.append(0.5 * np.sum((y-outputlayer_outputs)**2))
+
+        #calculating cost function values
+        J_theta.append((-1/m)*np.sum((y * np.log(outputlayer_inputs) + (1-y) * np.log(1-outputlayer_inputs))))
+        #J_theta.append(0.5 * np.sum((y-outputlayer_outputs)**2))
         p.append(i)
-
-
-
-        # for output layer
-
 
         # backward propogation
 
         # calculate error for last layer
         Error_output = outputlayer_outputs - y
+        #Error_output = (y * np.log(outputlayer_inputs) + (1-y) * np.log(1-outputlayer_inputs))
 
 
         # calculating derivative of output layer activations and hidden layer activations
@@ -71,85 +91,65 @@ def classify(iters, hidden_n, num_of_samples, X, theta, output_vars, y,bias_hidd
         bias_hidden = bias_hidden -  np.sum(delta_hidden, axis=0, keepdims=True)*alpha
         bias_output = bias_output - np.sum(delta_output, axis=0, keepdims=True)*alpha
 
-        #appending theta values for plotting
-        theta_1[10].append(bias_hidden[0][0])
-        theta_1[11].append(theta[1][0][0])
-        theta_1[12].append(theta[1][0][1])
-        theta_1[20].append(bias_hidden[0][1])
-        theta_1[21].append(theta[1][1][0])
-        theta_1[22].append(theta[1][1][1])
-
-        theta_2[10].append(bias_output[0][0])
-        theta_2[11].append(theta[2][0][0])
-        theta_2[12].append(theta[2][0][1])
-        theta_2[20].append(bias_output[0][1])
-        theta_2[21].append(theta[2][1][0])
-        theta_2[22].append(theta[2][1][1])
-
-    #plotting the cost function for each iteration
-    fig = plt.figure(figsize=(10,8))
-    ax = fig.add_subplot(111)
-    ax.set_title('Cost function plot')
-    ax.plot(p, J_theta, '.', color='blue')
-    plt.show()
-
-    # plotting the theta(1) values for each iteration
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111)
-    ax.set_title('Theta(1) values')
-    ax.plot(p,theta_1[10] , '.', color='blue',label = 'theta(1) 10')
-    ax.plot(p, theta_1[11], '.', color='red',label = 'theta(1) 11')
-    ax.plot(p, theta_1[11], '.', color='yellow',label = 'theta(1) 12')
-    ax.plot(p, theta_1[20], '.', color='green',label = 'theta(1) 20')
-    ax.plot(p, theta_1[21], '.', color='magenta',label = 'theta(1) 21')
-    ax.plot(p, theta_1[22], '.', color='orange',label = 'theta(1) 22')
-    ax.legend()
-    plt.show()
-
-    # plotting the theta(2) values for each iteration
-    fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111)
-    ax.set_title('Theta(2) values')
-    ax.plot(p, theta_2[10], '.', color='blue', label='theta(2) 10')
-    ax.plot(p, theta_2[11], '.', color='red', label='theta(2) 11')
-    ax.plot(p, theta_2[11], '.', color='yellow', label='theta(2) 12')
-    ax.plot(p, theta_2[20], '.', color='green', label='theta(2) 20')
-    ax.plot(p, theta_2[21], '.', color='magenta', label='theta(2) 21')
-    ax.plot(p, theta_2[22], '.', color='orange', label='theta(2) 22')
-    ax.legend()
-    plt.show()
-
-    return outputlayer_outputs
+    return bias_hidden,bias_output,theta
 
 
 
 def main():
-    X = np.array([[0.05,0.1]])
+    iris = datasets.load_iris()
+    X = iris.data[50:,2:]
+    X[:,0] = (X[:,0] - np.min(X[:,0])) / (np.max(X[:,0]) - np.min(X[:,0]))
+    X[:, 1] = (X[:, 1] - np.min(X[:, 1])) / (np.max(X[:, 1]) - np.min(X[:, 1]))
+    X1 = X
+    y = iris.target[50:]
+    for i in range(len(y)):
+        if y[i] == 1:
+            y[i] = 0
+        if y[i] == 2:
+            y[i] = 1
+    # X = np.array([[0.05,0.1]])
     inp_dim = len(X[0])
-    num_of_samples = len(X)
-    num_of_outputs = 2
-    hidden_n = 2
-    iterations = 100000
+    y = y.reshape(100,1)
+    y1 = y
+    error = 0
 
-    # Output
-    y = np.array([[0.01,0.99]])
+    #running the neural network 100 times using leave-one-out-analysis
+    for test in range(100):
+        print ("Test "),test+1
+        X = X1
+        y = y1
+        X_test = X[test]
+        Y_test = y[test]
+        X = np.delete(X,test,0)
+        y = np.delete(y,test,0)
+        num_of_samples = len(X)
+        num_of_outputs = 1
+        hidden_n = 2
+        iterations = 50000
 
-    #initialize theta values to random and uniformly distribited numbers from 0 to 1
-    theta = {1: np.random.uniform(size=(inp_dim,hidden_n)), 2: np.random.uniform(size=(hidden_n,num_of_outputs))}
+        #initialize theta values to random and uniformly distribited numbers from 0 to 1
+        theta = {1: np.random.uniform(size=(inp_dim,hidden_n)), 2: np.random.uniform(size=(hidden_n,num_of_outputs))}
 
-    #initialize bias to 1
-    bias_hidden = np.ones((1,hidden_n))
-    bias_output = np.ones((1,num_of_outputs))
-    #theta = {1:np.array([[-30,20,20],[10,-20,-20]]),2:np.array([[-10,20,20]])}
-    print theta[1].shape
-    print theta[2].shape
+        #initialize bias to 1
+        bias_hidden = np.ones((1,hidden_n))
+        bias_output = np.ones((1,num_of_outputs))
 
-    answers = classify(iterations, hidden_n, num_of_samples, X, theta, num_of_outputs, y,bias_hidden,bias_output)
-    print answers
+        bias_hidden,bias_output,theta = classify(iterations, hidden_n, num_of_samples, X, theta, num_of_outputs, y,bias_hidden,bias_output)
+        prob = test_classify(hidden_n, X_test, theta, num_of_outputs,bias_hidden, bias_output)
+        for i in prob:
+            if(i<0.5):
+                answers = 0
+            else:
+                answers = 1
+        print str(answers) + "\t" + str(Y_test[0]) + "\t" + str(prob)
+        if(answers != Y_test[0]):
+            error = error + 1
+    print error
+    average_error_rate = (error / 100.0)
+    print ("Average Error Rate:"),average_error_rate
 
 
 
 if __name__ == '__main__':
     main()
-
 
